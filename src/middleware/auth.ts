ba4f910a6633f178da7aca646234b1356bf87efe
amer <amertahawy111@gmail.com>
2025-04-18 15:04:14 +0200

    .

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from 'express';
-import User from '../models/User';
 
 export const addLocals = (req: Request, res: Response, next: NextFunction) => {
     res.locals.userId = req.session?.user?._id || null;
@@ -9,24 +8,12 @@ export const addLocals = (req: Request, res: Response, next: NextFunction) => {
     next();
 };
 
-export const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
-    if (req.session?.user?._id) {
-        try {
-            const user = await User.findById(req.session.user._id);
-            
-            if (!user) {
-                return res.redirect('/auth/login');
-            }
-            req.session.user = { _id: user._id, role: user.role }; // Update session with fresh data
-            next();
-        } catch (error) {
-            console.error('Error fetching user:', error);
-            res.redirect('/auth/login');
-        }
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    if (req.session?.user?._id) { // Check if user and _id exist
+        next();
     } else {
         res.redirect('/auth/login');
     }
-    console.log("isAuthenticated");
 };
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
